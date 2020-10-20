03b54857c8318c76b45da86a25c68c737b94a4a4
Iswandi Saputra <iswandi.saputra11@gmail.com>
2020-10-20 14:57:35 +0700

    Initial Commit

---
+++ OnedayFramework/OnedayFramework.h
@@ -0,0 +1,19 @@
+//
+//  OnedayFramework.h
+//  OnedayFramework
+//
+//  Created by Iswandi Saputra on 20/10/20.
+//  Copyright © 2020 inatech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnedayFramework.
+FOUNDATION_EXPORT double OnedayFrameworkVersionNumber;
+
+//! Project version string for OnedayFramework.
+FOUNDATION_EXPORT const unsigned char OnedayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnedayFramework/PublicHeader.h>
+
+
